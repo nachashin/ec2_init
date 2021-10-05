d4f3ebc7b535b4b831a801604e4387f2e36185c8
NAKAMURA Shingo <shingo@tsubata.itrev.info>
2021-10-05 14:47:53 +0900

    ec2_init.txt削除／ec2_init.sh修正

--- ec2_init.sh
+++ ec2_init.sh
@@ -6,8 +6,9 @@ yum -y update
 # individual setting
 H=home/ec2-user
 cd /tmp
-wget https://nachashin.github.io/ec2_init.tgz -O ec2_init.tgz
+wget https://nachashin.github.io/ec2_init.tgz
 tar xvfz ec2_init.tgz
+sleep 30
 cat $H/.ssh/authorized_keys >> /$H/.ssh/authorized_keys
 cat $H/.bashrc >> /$H/.bashrc
 for f in .inputrc .exrc .vimrc .bash_aliases
