ee4d7d95524f11c2b64a87a378caed0851cd3775
NAKAMURA Shingo <git@itrev.info>
2021-10-12 12:19:22 +0900

    ec2_mariadb.sh修正

--- ec2_mariadb.sh
+++ ec2_mariadb.sh
@@ -3,20 +3,20 @@ amazon-linux-extras install -y mariadb10.5
 systemctl enable mariadb
 systemctl start mariadb
 
-NEWPASS_ROOT=`cat /dev/urandom | tr -dc '[:alnum:]' | head -c 16`
-NEWPASS_WEB=`cat /dev/urandom | tr -dc '[:alnum:]' | head -c 16`
-WEBDBUSER=webdbuser
-WEBDB=webdb
+export NEWPASS_ROOT=`cat /dev/urandom | tr -dc '[:alnum:]' | head -c 16`
+export NEWPASS_WEB=`cat /dev/urandom | tr -dc '[:alnum:]' | head -c 16`
+export WEBDBUSER=webdbuser
+export WEBDB=webdb
 
-LOCALIP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-LOCALNET=`echo $LOCALIP | sed -e 's/^\([0-9]*\)\.\([0-9]*\)\.[0-9]*\.[0-9]*$/\1.\2.0.0/g'`
+export LOCALIP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+export LOCALNET=`echo $LOCALIP | sed -e 's/^\([0-9]*\)\.\([0-9]*\)\.[0-9]*\.[0-9]*$/\1.\2.0.0/g'`
 
 sed -i -e "s/P@ssw0rd123/${NEWPASS_ROOT}/g" /root/.bash_aliases
 echo "alias myweb=\"mysql -u web -p'${NEWPASS_WEB}' ${WEBDBUSER}\"" >> /root/.bash_aliases
 
 cat << EOS > /etc/my.cnf.d/custom.cnf
 [mysqld]
-aracter-set-server=utf8mb4
+character-set-server=utf8mb4
 skip-character-set-client-handshake
 default-storage-engine=InnoDB
 
@@ -27,8 +27,11 @@ default-character-set=utf8mb4
 default-character-set=utf8mb4
 EOS
 
+systemctl restart mariadb
+
 mysql -u root -e "
-    UPDATE mysql.user SET Password=PASSWORD('${NEWPASS_ROOT}') WHERE User='root';
+    -- UPDATE mysql.user SET Password=PASSWORD('${NEWPASS_ROOT}') WHERE User='root';
+    SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${NEWPASS_ROOT}');
     DELETE FROM mysql.user WHERE User='';
     DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
     DROP DATABASE test;
